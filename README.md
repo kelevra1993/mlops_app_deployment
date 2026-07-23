cbc9cef7670e1b39f5e38c6639f7e55acc1841d8
Robert Kelevra <kelevra.robert@gmail.com>
2026-07-23 18:40:09 +0200

    Upload Terraform For Infrastructure

--- README.md
+++ README.md
@@ -1,4 +1,29 @@
 
+Gcloud Steps : 
+- Revoke everything before starting
+  - gcloud auth revoke --all
+- Connect to gcloud via cli
+  - gcloud auth login
+- If a project is set and you want to remove it
+  - gcloud config unset project
+- Set up application default credentials
+  - gcloud auth application-default login
+
+
+Terraform Steps On Mac:
+- Install Hashicorp Tap :
+  - brew tap hashicorp/tap
+- Install Terraform
+  - brew install hashicorp/tap/terraform
+- Test it out
+  - terraform -help
+- For autocomplete package
+  - terraform -install-autocomplete
+- Initialize working directory
+  - terraform init
+
+
+
 Next Steps :
 
 - Make Gradio App
---
+++ terraform/main.tf
@@ -0,0 +1,222 @@
+# 0. Enable Required Google Cloud APIs
+resource "google_project_service" "enabled_apis" {
+  for_each = toset([
+    "compute.googleapis.com",             # For VPC, Subnets, NAT, and VMs
+    "container.googleapis.com",           # For Kubernetes Engine
+    "artifactregistry.googleapis.com",    # For the Docker repository
+    "bigquery.googleapis.com",            # For the prediction history dataset/table
+    "iam.googleapis.com",                 # For creating the custom Service Account
+    "cloudresourcemanager.googleapis.com" # For assigning IAM roles to the Service Account
+  ])
+
+  project = "ml-ops-classifier-app"
+  service = each.key
+
+  # We set this to false so that running 'terraform destroy' does not accidentally
+  # disable the APIs before it finishes deleting the infrastructure that relies on them.
+  disable_on_destroy = false
+}
+# 1. Definition of the Virtual Private Cloud Network
+resource "google_compute_network" "machine_learning_virtual_private_network" {
+  name                    = "machine-learning-virtual-private-network"
+  auto_create_subnetworks = false
+
+  # Wait for the APIs to be enabled before creating the network
+  depends_on = [google_project_service.enabled_apis]
+}
+
+# 2. Define a Subnetwork for the Kubernetes Cluster
+resource "google_compute_subnetwork" "kubernetes_sub_network" {
+  name          = "kubernetes-sub-network"
+  ip_cidr_range = "10.0.0.0/16"
+  region        = "europe-west1"
+  network       = google_compute_network.machine_learning_virtual_private_network.id
+}
+
+# 3. Define the Google Kubernetes Engine (GKE) Cluster (Control Plane only)
+#    - Zonal cluster to save costs for learning
+resource "google_container_cluster" "primary_cluster" {
+  name     = "machine-learning-cluster"
+  location = "europe-west1-b"
+
+  # Attach it to the VPC and Subnet created above
+  network    = google_compute_network.machine_learning_virtual_private_network.id
+  subnetwork = google_compute_subnetwork.kubernetes_sub_network.id
+
+  # We disable deletion protection so we can easily destroy and recreate our learning cluster when needed.
+  deletion_protection = false
+
+  # Delete the default node pool created by GCP so we can define a dedicated node pool later
+  remove_default_node_pool = true
+
+  # Required by the Google API: even though we delete the default pool immediately,
+  # the API requires setting an initial count of at least 1 node during creation.
+  initial_node_count = 1
+
+  # Turning this empty block on enables VPC-Native routing (Alias IPs).
+  # This ensures every Kubernetes Pod gets its own real, routable IP address from our subnet.
+  ip_allocation_policy {}
+
+  private_cluster_config {
+    # Hides worker VMs from the internet. They receive ONLY private internal IP addresses.
+    enable_private_nodes = true
+
+    # Controls access to the Kubernetes API server (Control Plane / Master).
+    # Setting to 'false' assigns a public IP to the API server so we can run 'kubectl' from our local Machines.
+    enable_private_endpoint = false
+
+    # Reserved private IP range (/28 = 16 IPs) for Google to host the managed Control Plane.
+    # Must NOT overlap with our VPC network range (10.0.0.0/16).
+    # [/28 meaning 28 bits locked so 2^4 available ip's]
+    master_ipv4_cidr_block = "172.16.0.0/28"
+  }
+
+}
+
+
+# 4. Create a Custom Service Account for the GKE Nodes
+resource "google_service_account" "kubernetes_node_service_account" {
+  account_id   = "ml-node-service-account"
+  display_name = "Custom Service Account for GKE Nodes"
+}
+
+# 5. Assign IAM Roles to the Custom Service Account
+# We use google_project_iam_member to grant specific permissions to our service account.
+# Allows our nodes to send system and application logs to Cloud Logging
+resource "google_project_iam_member" "node_service_account_logging" {
+  project = "ml-ops-classifier-app"
+  role    = "roles/logging.logWriter"
+  member  = "serviceAccount:${google_service_account.kubernetes_node_service_account.email}"
+}
+
+# Allows our nodes to send metrics to Cloud Monitoring (Grafana/Prometheus)
+resource "google_project_iam_member" "node_service_account_monitoring" {
+  project = "ml-ops-classifier-app"
+  role    = "roles/monitoring.metricWriter"
+  member  = "serviceAccount:${google_service_account.kubernetes_node_service_account.email}"
+}
+
+# Allows our nodes to pull Docker images from Artifact Registry / Container Registry
+resource "google_project_iam_member" "node_service_account_registry" {
+  project = "ml-ops-classifier-app"
+  role    = "roles/artifactregistry.reader"
+  member  = "serviceAccount:${google_service_account.kubernetes_node_service_account.email}"
+}
+
+# Allows our nodes to read and write files (like user images) to Cloud Storage buckets
+resource "google_project_iam_member" "node_service_account_storage" {
+  project = "ml-ops-classifier-app"
+  role    = "roles/storage.objectAdmin"
+  member  = "serviceAccount:${google_service_account.kubernetes_node_service_account.email}"
+}
+
+# Allows our nodes to read and write prediction data to BigQuery tables
+resource "google_project_iam_member" "node_service_account_bigquery" {
+  project = "ml-ops-classifier-app"
+  role    = "roles/bigquery.dataEditor"
+  member  = "serviceAccount:${google_service_account.kubernetes_node_service_account.email}"
+}
+
+# 6. Define the Custom Node Pool (The Worker Virtual Machines)
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "machine-learning-node-pool"
+  location   = "europe-west1-b"
+  cluster    = google_container_cluster.primary_cluster.name
+  node_count = 2
+
+  # Define the machine types and set the gpu's
+  node_config {
+    machine_type = "n1-standard-4"
+    disk_size_gb = 100
+
+    guest_accelerator {
+      type  = "nvidia-tesla-t4"
+      count = 1
+    }
+
+    # Attach our custom Service Account to the nodes
+    service_account = google_service_account.kubernetes_node_service_account.email
+
+    # Using the single "cloud-platform" master scope, we instead rely entirely on the
+    # fine-grained IAM roles we explicitly granted to the Service Account above.
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    # Free-text 'hashtags' stamped onto our VMs so our firewall rules can target them specifically later.
+    tags = ["google-kubernetes-engine-node", "machine-learning-ops-cluster"]
+  }
+}
+
+# 7. Create a Google Cloud Storage Bucket for User Images
+resource "google_storage_bucket" "image_storage_bucket" {
+  # WARNING: Cloud Storage bucket names must be GLOBALLY unique across all of Google Cloud.
+  # You might need to add some random numbers to the end of this name so it doesn't clash.
+  name     = "machine-learning-ops-images-bucket-2026"
+  location = "europe-west1"
+
+  # Setting this to true means if we ever want to destroy our project,
+  # Terraform is allowed to delete this bucket even if there are images inside it.
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
+# 8. Create a BigQuery Dataset (The container for our tables)
+resource "google_bigquery_dataset" "prediction_dataset" {
+  dataset_id = "machine_learning_predictions"
+  location   = "europe-west1"
+
+  # Equivalent to force_destroy. Allows Terraform to delete this dataset
+  # even if it still contains tables.
+  delete_contents_on_destroy = true
+}
+
+# 9. Create the BigQuery Table and define its columns
+resource "google_bigquery_table" "prediction_history_table" {
+  dataset_id = google_bigquery_dataset.prediction_dataset.dataset_id
+  table_id   = "inference_history"
+
+  # Terraform defaults this to true for BigQuery tables as a safety measure.
+  # We set it to false so we can successfully run 'terraform destroy' later.
+  deletion_protection = false
+
+  schema = <<EOF
+    [
+      {"name": "uuid", "type": "STRING", "mode": "REQUIRED"},
+      {"name": "predicted_class", "type": "STRING", "mode": "NULLABLE"},
+      {"name": "probability", "type": "FLOAT", "mode": "NULLABLE"},
+      {"name": "timestamp", "type": "TIMESTAMP", "mode": "NULLABLE"},
+      {"name": "kubernetes_node", "type": "STRING", "mode": "NULLABLE"},
+      {"name": "gcs_image_uri", "type": "STRING", "mode": "NULLABLE"},
+      {"name": "additional_comment", "type": "STRING", "mode": "NULLABLE"}
+    ]
+    EOF
+}
+
+# 10. Create an Artifact Registry Repository for Docker Images
+resource "google_artifact_registry_repository" "docker_repository" {
+  location      = "europe-west1"
+  repository_id = "machine-learning-artifacts-registry"
+  description   = "Docker repository for Gradio and Triton container images"
+  format        = "DOCKER"
+}
+
+# 11. Create a Cloud Router (Required by Cloud Network Address Translation [NAT])
+resource "google_compute_router" "network_address_translation_router" {
+  name    = "machine-learning-network-address-translation-router"
+  region  = "europe-west1"
+  network = google_compute_network.machine_learning_virtual_private_network.id
+}
+
+# 12. Create a Cloud NAT Gateway (Allows private VMs to access the internet)
+# One vm tries to download an element X, it's request first goes to the NAT's gateway, the gateway takes that request
+# strips it's information, appends it's own public IP, sends the request outside for the element X, when the element X
+# is returned to the gateway, the gateway looks for which VM asked for X and send it to that specific VM.
+resource "google_compute_router_nat" "nat_gateway" {
+  name                               = "machine-learning-network-address-translation-gateway"
+  router                             = google_compute_router.network_address_translation_router.name
+  region                             = google_compute_router.network_address_translation_router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+}
