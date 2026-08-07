26fc4f2c549dad9bf3aebd30b0e823a16b2af412
Robert Kelevra <kelevra.robert@gmail.com>
2026-08-07 16:31:42 +0200

    [Refactoring] Add predicted information columns to constants.

--- app/utilities/constants.py
+++ app/utilities/constants.py
@@ -1,4 +1,3 @@
-
 # Simple Expected Image File Extensions
 IMAGE_FILE_EXTENSIONS = (".png", ".jpg", ".jpeg", ".gif", ".webp", ".tiff")
 
@@ -9,6 +8,13 @@ PROJECT_ID = "ml-ops-classifier-app"
 DATASET_ID = "machine_learning_predictions_euw3"
 TABLE_ID = "inference_history"
 TABLE_REFERENCE = f"{PROJECT_ID}.{DATASET_ID}.{TABLE_ID}"
+PREDICTED_INFROMATION_COLUMNS = ["uuid",
+                                 "predicted_class",
+                                 "probability",
+                                 "timestamp",
+                                 "kubernetes_node",
+                                 "gcs_image_uri",
+                                 "additional_comment"]
 
 # Google Cloud Storage Constants
 BUCKET_NAME = "machine-learning-ops-images-bucket-2026"
